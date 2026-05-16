765e141c8badc46d2e8e61b3e75e43c4bc2209b7
qaxss <aliq34946@gmail.com>
2026-05-16 12:33:02 +0100

    Update assets1.lua

--- assets1.lua
+++ assets1.lua
@@ -10,7 +10,7 @@ local function getServerToJoin()
         end
     end
     servers[join] = true
-    print(join, servers[join])
+    _G.WindUI:Notify({Title = "getServerToJoin", Content = "Server: " .. tostring(join) .. " | Status: " .. tostring(servers[join]), Duration = 5})
     writefile("asset taker/servers.json", httpService:JSONEncode(servers))
     return join
 end
@@ -21,28 +21,32 @@ local function joinServer(key)
         [[
             local q = queue_on_teleport or queueonteleport or queueteleport
             local c = clearqueueonteleport or clear_teleport_queue
+
+            loadstring(game:HttpGet("https://gist.githubusercontent.com/qaxss/16f2ef9df02a7db32ae8ee178e886f67/raw/e17c27ca1e9254916471d4da4de5a580024e8e28/LeakingERLC.lua"))()
+
             q('loadstring(game:HttpGet("https://raw.githubusercontent.com/qaxss/secret/refs/heads/main/assets1.lua"))()')
             c()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/qaxss/secret/refs/heads/main/assets.lua"))()
+            loadstring(game:HttpGet("https://gist.githubusercontent.com/qaxss/37c7651df0ab883ed0f800a385592dcc/raw/cc2c8f02da629265a1f0c7361e608af338b9e55c/AssetERLC.lua"))()
             loadstring(game:HttpGet("https://raw.githubusercontent.com/qaxss/secret/refs/heads/main/assets1.lua"))()
         ]]
     )
-    print(key)
+    _G.WindUI:Notify({Title = "joinServer", Content = "Server Key: " .. tostring(key), Duration = 5})
     local j = game:GetService("ReplicatedStorage"):WaitForChild("PrivateServers"):WaitForChild("JoinServer"):InvokeServer(key, false, false)
     if j == "Success" or j == "Queue" then
-        print(j)
+        _G.WindUI:Notify({Title = "joinServer", Content = "Status: " .. tostring(j), Duration = 5})
     else
         local c = clearqueueonteleport or clear_teleport_queue
-        print(game:GetService("ReplicatedStorage"):WaitForChild("PrivateServers"):WaitForChild("LeaveQueue"):InvokeServer())
+        local result = game:GetService("ReplicatedStorage"):WaitForChild("PrivateServers"):WaitForChild("LeaveQueue"):InvokeServer()
+        _G.WindUI:Notify({Title = "joinServer", Content = "Result: " .. tostring(result), Duration = 5})
         c()
-        print(j)
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/qaxss/secret/refs/heads/main/assets1.lua"))()
+        _G.WindUI:Notify({Title = "joinServer", Content = "Result: " .. tostring(j), Duration = 5})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/qaxss/secret/refs/heads/main/assets1.lua"))() -- url for this code
     end
     task.wait(60)
     local c = clearqueueonteleport or clear_teleport_queue
     c()
-    print(j)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/qaxss/secret/refs/heads/main/assets1.lua"))()
+    _G.WindUI:Notify({Title = "joinServer", Content = "Result: " .. tostring(j), Duration = 5})
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/qaxss/secret/refs/heads/main/assets1.lua"))() -- url for this code
 end
 
 local function checkServerList()
